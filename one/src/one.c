220d5ea144d6c00297b36023f9433a58012e332f
Aravindgopalavarma <aravind.datla@okstate.edu>
2015-11-17 23:18:11 -0600

    changed one

--- one/src/one.c
+++ one/src/one.c
@@ -21,7 +21,7 @@ int main(int argc, char **argv) {
 		printf("")
 	}*/
 	//printf("%s",buffer);
-	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
+	puts("!!!Hello World excels!!!"); /* prints !!!Hello World!!! */
 	return EXIT_SUCCESS;
 }
 
